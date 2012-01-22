0180b176dbecc185bc1ff655e992451fde7a7d57
roninmoto <roninmoto@gmail.com>
2012-01-22 09:31:06 -0500

    added JSON data for testing

---
+++ js/JSON.js
@@ -0,0 +1,29 @@
+var json = {
+    "appt1":{
+        "group": ["Selection:", "Personal"],
+        "range": ["Level of Importance", "10"],
+        "name": ["Name:", "Jose Bandersmit"],
+        "date": ["Date:", "01-01-2012"],
+        "tom": ["Time of Meeting:", "1200"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "This is a test note."]
+    },
+    "appt2":{
+        "group": ["Selection:", "Business"],
+        "range": ["Level of Importance", "6"],
+        "name": ["Name:", "Capt Morgan"],
+        "date": ["Date:", "01-15-2012"],
+        "tom": ["Time of Meeting:", "1300"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Bring Rum."]
+    },
+    "appt3":{
+        "group": ["Selection:", "Other"],
+        "range": ["Level of Importance", "2"],
+        "name": ["Name:", "Ronald McDonald"],
+        "date": ["Date:", "01-20-2012"],
+        "tom": ["Time of Meeting:", "1600"],
+        "available": ["Available:", "Yes"],
+        "notes": ["Notes:", "Big Macs for everyone!"]
+    }    
+}
